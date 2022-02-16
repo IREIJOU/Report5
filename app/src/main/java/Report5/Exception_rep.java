1d66a871f422b78098e84b005b8ee34574e4ad2a
Shunya IREIJO <e205760@ie.u-ryukyu.ac.jp>
2022-02-16 20:22:54 +0900

    ステップ3でのコミット

--- app/src/main/java/Report5/Exception_rep.java
+++ app/src/main/java/Report5/Exception_rep.java
@@ -1,15 +1,8 @@
 package Report5;
 
-import java.lang.NullPointerException;
-
 public class Exception_rep {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生したよ。");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }   
 }
